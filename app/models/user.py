0eb031bdb86579db806a2547fe4a2ebeb1f7e0cc
Ildus <ildusnurlygayanov@gmail.com>
2026-03-09 17:24:05 +0500

    Add добавил хэш для пароля в user model

--- app/models/user.py
+++ app/models/user.py
@@ -4,3 +4,4 @@ from sqlmodel import SQLModel, Field
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str
+    password_hash: Optional[str] = Field(default=None)
